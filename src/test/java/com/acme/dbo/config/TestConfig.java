edec7201912779c8134a212a2d76ea18d372ef43
eugene-krivosheyev <eugene.krivosheyev@gmail.com>
2020-05-04 12:24:29 +0300

    Update Selenium driver setup.

--- src/test/java/com/acme/dbo/config/TestConfig.java
+++ src/test/java/com/acme/dbo/config/TestConfig.java
@@ -8,14 +8,28 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
+import java.util.ArrayList;
+
+import static java.lang.System.*;
+
 
 @TestConfiguration
 public class TestConfig {
     @Lazy @Bean
     @Profile("it")
     public WebDriver webDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver(new ChromeOptions()
+        if (driver != null) return driver;
+
+        //region Driver setup
+        String chromeDriverEnvLocation = getenv("webdriver.chrome.driver");
+        String chromeDriverPropLocation = getProperty("webdriver.chrome.driver");
+        if (chromeDriverEnvLocation != null || chromeDriverPropLocation != null) {
+            setProperty("webdriver.chrome.driver", chromeDriverEnvLocation != null ? chromeDriverEnvLocation : chromeDriverPropLocation);
+        } else {
+            WebDriverManager.chromiumdriver().setup();
+        }
+
+        driver = new ChromeDriver(new ChromeOptions()
                 .addArguments("--headless")
                 .addArguments("--disable-gpu")
                 .addArguments("--disable-dev-shm-usage")
@@ -24,7 +38,19 @@ public class TestConfig {
                 .addArguments("--start-maximized")
                 .addArguments("--window-size=800,2000")
                 .addArguments("--ignore-certificate-errors")
-
         );
+        //endregion
+
+        //region Performance hack setup
+        //always keep open tab to prevent closing driver at <code>driver.close()</code>
+        //note that <code>driver.quit()</code> closes driver anyway
+        driver.manage().window().maximize();
+
+        ((JavascriptExecutor)driver).executeScript("window.open()");
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        //endregion
+
+        return driver;
     }
 }

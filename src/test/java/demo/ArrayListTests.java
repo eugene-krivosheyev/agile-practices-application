659b4e2cbe8a35edbf770342dce3f30f2928afba
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-29 15:45:01 +0300

    Add Hamcrest demo.

--- src/test/java/demo/ArrayListTests.java
+++ src/test/java/demo/ArrayListTests.java
@@ -1,19 +1,32 @@
 package demo;
 
 import com.gargoylesoftware.htmlunit.Cache;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.junit.Assert.*;
 
 public class ArrayListTests {
+    private ArrayList<Object> sut;
+
+    public ArrayListTests() {
+        System.out.println("Tst instance cretaed!!!!");
+    }
+
+    @Before
+    public void setUp() {
+        sut = new ArrayList<>();
+    }
+
     @Test
     public void shouldSizeIncrementedAndContainsElementWhenElementAdded() {
         //region Fixture | Arrange | Given
-        final ArrayList<Object> sut = new ArrayList<>();
-        final Object dummy = new Object();
+        final String dummy = "?????";
         //endregion
 
         //region Act | When
@@ -23,6 +36,18 @@ public class ArrayListTests {
         //region Assert | Then
         assertEquals("", 1, sut.size());
         assertTrue(sut.contains(dummy));
+
+        assertThat(sut, allOf(
+                hasItem("null"),
+                nullValue()
+        ));
         //endregion
+    }
+
+    @Test
+    public void shouldSizeIncrementedAndContainsElementWhenNullElementAdded() {
+        sut.add(null);
+        assertEquals(1, sut.size());
+        assertTrue(sut.contains(null));
     }
 }
